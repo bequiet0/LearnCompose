ca075a8ca1af9b4905980f9d2f43a4ed6de7f0cc
RYAN_Laptop <goldwayneeee@gmail.com>
2024-03-16 10:51:34 +0300

    Second Lesson (Контейнеры, расположение, modifier, column, row)

--- NecoRu/app/src/main/java/com/example/composelesson/MainActivity.kt
+++ NecoRu/app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composelesson.ui.theme.ComposeLessonTheme
 
@@ -16,14 +24,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewText()
+            Row(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxWidth().fillMaxHeight(0.5f)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .background(Color.Green).fillMaxWidth(0.5f).fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Hello!")
+                    Text(text = "Girona!")
+                    Text(text = "Madrid!")
+                }
+                Column(
+                    modifier = Modifier
+                        .background(Color.Blue).fillMaxHeight().fillMaxWidth(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Hello!")
+                    Text(text = "Girona!")
+                    Text(text = "Madrid!")
+                }
+            }
+
         }
     }
 }
 
-@Preview (showBackground = true)
-@Composable
-fun NewText(){
-    Text(text = "Hello!")
-
-}
