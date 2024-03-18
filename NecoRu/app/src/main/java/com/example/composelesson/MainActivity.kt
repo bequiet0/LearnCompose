61272d5e6ac002d7408a9e3fdcae12c0d2fc1e49
RYAN_PC <goldwayneeee@gmail.com>
2024-03-18 19:13:58 +0300

    Third lesson (card, box)

--- NecoRu/app/src/main/java/com/example/composelesson/MainActivity.kt
+++ NecoRu/app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -3,55 +3,80 @@ package com.example.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelesson.ui.theme.ComposeLessonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth().fillMaxHeight(0.5f)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .background(Color.Green).fillMaxWidth(0.5f).fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Girona!")
-                    Text(text = "Madrid!")
-                }
-                Column(
-                    modifier = Modifier
-                        .background(Color.Blue).fillMaxHeight().fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Girona!")
-                    Text(text = "Madrid!")
-                }
-            }
+            Column() {
+                ListItem(name = "Emili", prof = "Smith")
+                ListItem(name = "Emili", prof = "Smith")
 
+
+            }
         }
     }
+}
+
+@Composable
+private fun ListItem(name: String, prof:  String){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Column {
+
+        }
+        Box() {
+            Row(
+                verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+
 }
 
